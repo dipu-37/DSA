d3a2cc0139e0a526ad11ba8bccafbb3bb6064455
dipu-37 <razibul1105nstu@gmail.com>
2024-11-15 21:53:35 +0600

    first commit

--- pattern.cpp
+++ pattern.cpp
@@ -76,11 +76,35 @@ void pattern12(int n)
         cout << endl;
         space -= 2;
     }
-// 1        1
-// 12      21
-// 123    321
-// 1234  4321
-// 1234554321
+    // 1           1
+    // 12      21
+    // 123    321
+    // 1234  4321
+    // 1234554321
+}
+void pattern13(int n)
+{
+
+    int start = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << start <<" ";
+            start ++;
+        }
+        cout<<endl;
+    }
+
+//     5
+// 1 
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
 }
 
 int main()
@@ -90,5 +114,5 @@ int main()
     int n;
     cin >> n;
 
-    pattern12(n);
+    pattern13(n);
 }
