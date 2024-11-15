233e54752b2462914d843ce0a8600dd91d6b7fea
dipu-37 <razibul1105nstu@gmail.com>
2024-11-15 21:42:46 +0600

    first commit

--- pattern.cpp
+++ pattern.cpp
@@ -31,16 +31,50 @@ void pattern10(int n)
 
 void pattern11(int n)
 {
-    int start =1;
-    for(int i=0; i<n ; i++){
-        if( i%2 ==0) start=1;
-        else start = 0;
-        for(int j=0; j<=i; j++){
-            cout<<start;
-            start = 1-start;
+    int start = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+            start = 1;
+        else
+            start = 0;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << start;
+            start = 1 - start;
         }
 
-        cout<<endl;
+        cout << endl;
+    }
+    //     5
+    // 1
+    // 01
+    // 101
+    // 0101
+    // 10101
+}
+
+void pattern12(int n)
+{
+    int space = 2 * (n - 1);
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+        space -= 2;
     }
 }
 
@@ -51,5 +85,5 @@ int main()
     int n;
     cin >> n;
 
-    pattern11(n);
+    pattern12(n);
 }
