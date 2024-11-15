00b7f28659c730294060c77bafba4c70d8f31149
dipu-37 <razibul1105nstu@gmail.com>
2024-11-15 21:33:24 +0600

    first commit

---
+++ note.cpp
@@ -0,0 +1,13 @@
+
+// 1. for pattern related node
+
+{
+    // 1 2 3 4 5 -- -- -- > 5 4 3 2 1 == i = (n - i)
+    // 1 3 5 7 9 -- -- -- > 9 7 5 3 1 == 2 *i + 1 = 2 * n - (2 * i + 1)
+    // 2 *n = 2 * n - 1
+    // 6 4 2 0 ----> first = 2(n-1)   first =-2;   
+
+    // flip :
+    // start =1;
+    // start = 1- start    
+}
