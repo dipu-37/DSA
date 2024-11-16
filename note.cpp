3cd27b1c6732fab16521b7479d6e4979cbef2821
dipu-37 <razibul1105nstu@gmail.com>
2024-11-17 01:10:36 +0600

    first commit

--- note.cpp
+++ note.cpp
@@ -1,5 +1,5 @@
 
-// 1. for pattern related node
+
 
 {
     // 1 2 3 4 5 -- -- -- >  i = (n - i)
@@ -19,42 +19,14 @@
     // 'A'+i = 'z'  // loop finish after 'z'
     // 'A'+i = 'A' + (n-i)  : A B C D E F
 
-    {
-        // vector :
-        
-        // sort ( v.begin(), v.end() );
-        // int Sz = unique ( v.begin(), v.end() ) - v.begin();  // 3
-        // for ( int i = 0; i < sz; i++ ) cout << a[i] << " "; /// 1 2 3
+    // digit : 
+    // 1234 % 10 = always give last_digit(4);
+    // 1234 / 10 = always remove last_digit; 
+    // reverse = reverse *10 + last_digit;   123=321
+    // count_digit = (int) (log10(n)+1)
+    // (n != 0) = negative + posative ;  (n > 0) = only postive 
+    // palindorm = (original == reverse)
+    // armstrong = (original == sum of digit cube) 1^3 + 2^3 + 3^3
 
-        // max_element( a.begin(), a.end() ) - a.begin()  // return index;
-        // *max_element( a.begin(), a.end() )  // return value ;
-    }
-
-    {
-        // vector of pair :
-
-        // bool cmp(const pair<int, int> &p1, const pair<int, int> &p2)
-        // {
-        //     if (p1.first > p2.first)
-        //         return 1;   
-        //     if (p1.first == p2.first)
-        //         return (p1.second < p2.second);
-        //     return 0;
-        // }
-
-        // int main()
-        // {
-        //     int x, y;
-        //     int n;
-        //     cin >> n;
-        //     vector<pair<int, int>> v;
-        //     for (int i = 0; i < n; i++)
-        //     {
-        //         cin >> x >> y;
-        //         v.push_back({x, y});
-        //     }
-        // }
-        //sort ( v.begin(), v.end(), cmp ); 
-    }
 
 }
--- pattern.cpp
+++ pattern.cpp
@@ -1,235 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void pattern10(int n)
-{
-    for (int i = 0; i < 2 * n; i++)
-    {
-        int start = i;
-        if (i > n)
-            start = 2 * n - i;
-        for (int j = 0; j < start; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
 
-        // follow this input and output
-        // 5
-
-        // *
-        // **
-        // ***
-        // ****
-        // *****
-        // ****
-        // ***
-        // **
-        // *
-    }
-}
-
-void pattern11(int n)
-{
-    int start = 1;
-    for (int i = 0; i < n; i++)
-    {
-        if (i % 2 == 0)
-            start = 1;
-        else
-            start = 0;
-        for (int j = 0; j <= i; j++)
-        {
-            cout << start;
-            start = 1 - start;
-        }
-
-        cout << endl;
-    }
-    //     5
-    // 1
-    // 01
-    // 101
-    // 0101
-    // 10101
-}
-
-void pattern12(int n)
-{
-    int space = 2 * (n - 1);
-    for (int i = 1; i <= n; i++)
-    {
-
-        for (int j = 1; j <= i; j++)
-        {
-            cout << j;
-        }
-        for (int j = 1; j <= space; j++)
-        {
-            cout << " ";
-        }
-
-        for (int j = i; j >= 1; j--)
-        {
-            cout << j;
-        }
-        cout << endl;
-        space -= 2;
-    }
-    // 1           1
-    // 12      21
-    // 123    321
-    // 1234  4321
-    // 1234554321
-}
-
-void pattern14(int N)
-
-{
-
-    // Outer loop for the number of rows.
-    for (int i = 0; i < N; i++)
-    {
-
-        // Inner loop will loop for i times and
-        // print alphabets from A to A + i.
-        for (char ch = 'A'; ch <= 'A' + i; ch++)
-        {
-            cout << ch << " ";
-        }
-        // As soon as the letters for each iteration are printed, we move to the
-        // next row and give a line break otherwise all letters
-        // would get printed in 1 line.
-        cout << endl;
-    }
-}
-
-void pattern16(int N)
-{
-
-    // Outer loop for the number of rows.
-    for (int i = 0; i < N; i++)
-    {
-
-        // Defining character for each row.
-        char ch = 'A' + i;
-        for (int j = 0; j <= i; j++)
-        {
-
-            // same char is to be printed i times in that row.
-            cout << ch << " ";
-        }
-        // As soon as the letters for each iteration are printed, we move to the
-        // next row and give a line break otherwise all letters
-        // would get printed in 1 line.
-        cout << endl;
-    }
-}
-void pattern17(int n)
-{
-
-    for (int i = 0; i < n; i++)
-    {
-        // char ch = 'A'+i;
-        for (int j = 0; j < n - i; j++)
-        {
-            cout << " ";
-        }
-
-        char ch = 'A';
-        int breakpoint = (2 * i + 1) / 2;
-
-        for (int j = 1; j <= 2 * i + 1; j++)
-        {
-
-            cout << ch;
-            if (j <= breakpoint)
-                ch++;
-            else
-                ch--;
-        }
-        for (int j = 0; j < n - i; j++)
-        {
-            cout << " ";
-        }
-        cout << endl;
-    }
-    //      A
-    //     ABA
-    //    ABCBA
-    //   ABCDCBA
-    //  ABCDEDCBA
-}
-
-void pattern18(int n)
-{
-
-    for (int i = 0; i < n; i++)
-    {
-
-        for (char ch = ('A' + n - 1) - i; ch <= ('A' + n - 1); ch++)
-        {
-
-            cout << ch << " ";
-        }
-
-        cout << endl;
-    }
-    //     5
-    // E
-    // D E
-    // C D E
-    // B C D E
-    // A B C D E
-}
-void pattern19(int n)
-{
-    int space = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        for(int j=1; j<=n-i; j++){
-            cout<<"*";
-        }
-        for(int j=0; j<space; j++){
-            cout<<" ";
-        }
-        for(int j=1; j<=n-i; j++){
-            cout<<"*";
-        }
-        space +=2;
-
-        cout << endl;
-    }
-
-    int space2 = 2*n-2;
-    for (int i = 1; i <= n; i++)
-    {
-        for(int j=1; j<= i; j++){
-            cout<<"*";
-        }
-        for(int j=0; j<space2; j++){
-            cout<<" ";
-        }
-        for(int j=1; j<= i; j++){
-            cout<<"*";
-        }
-        space2 -=2;
-
-        cout << endl;
-    }
-    5
-// **********
-// ****   ****
-// ***      ***
-// **         **
-// *            *
-// *            *
-// **         **
-// ***      ***
-// ****   ****
-// **********
-}
 int main()
 {
 
@@ -237,5 +9,20 @@ int main()
     int n;
     cin >> n;
 
-    pattern19(n);
+    int duplicate = n;
+    int sum = 0;
+
+    while (n > 0)
+    {
+        int lastdigit = n % 10;
+        n = n / 10;
+        sum = sum + (lastdigit * lastdigit * lastdigit);
+    }
+    if (duplicate == sum)
+    {
+        cout << "yes";
+    }
+    else {
+        cout << "no";
+    }
 }
