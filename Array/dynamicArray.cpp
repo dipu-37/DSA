15dd47b98562355525bebba3b106918d757a300f
Razibul Hasan Dipu <razibul1105nstu@gmail.com>
2025-06-22 17:57:02 +0600

    dynamic array

---
+++ Array/dynamicArray.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = i;
+    }
+
+    int *brr = new int[n * 3];
+    for (int i = 0; i < n; i++)
+    {
+        brr[i] = arr[i];
+    }
+
+    delete[] arr;
+    for (int i = 0; i < n * 3; i++)
+    {
+        cout << brr[i] << " ";
+    }
+
+    return 0;
+}
