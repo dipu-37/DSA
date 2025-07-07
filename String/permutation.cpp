fd933da97b6a339e4a44bb90d9ef3e7c2567feae
Razibul Hasan Dipu <razibul1105nstu@gmail.com>
2025-07-07 11:28:02 +0600

    permutation of string

---
+++ String/permutation.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isFrqSame(int frq1[], int frq2[])
+{
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (frq1[i] != frq2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int frq1[26] = {0};
+    for (int i = 0; i < s1.size(); i++)
+    {
+        int inx = s1[i] - 'a';
+        frq1[inx]++;
+    }
+    // for (int i = 0; i < 26; i++)
+    // {
+    //     if (frq1[i] > 0)
+    //     {
+    //         cout << char(i + 'a') << frq1[i] << endl;
+    //     }
+    // }
+
+    int windowSize = s1.size();
+    int frq2[26] = {0};
+    for (int i = 0; i < s2.size(); i++)
+    {
+
+        // for (int j = 0; j < windowSize; j++)
+        // {
+        //     cout << i + j << endl;
+        //     frq2[s2[i + j] - 'a']++;
+        // }
+
+       // int frq2[26] = {0};
+
+        int windowInx = 0;
+        int inx = i;
+        while (windowInx < windowSize && inx < s2.size())
+        {
+            frq2[s2[inx] - 'a']++;
+            windowInx++;
+            inx++;
+        }
+        if (isFrqSame(frq1, frq2))
+        {
+            return true;
+        }
+        memset(frq2, 0, sizeof(frq2));
+    }
+}
